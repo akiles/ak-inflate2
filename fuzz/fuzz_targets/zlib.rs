fd5712750bca9e2deb80a02ac12ad7720f8fb2d6
Ulf Lilleengen <ulf@digili.no>
2025-04-04 08:41:43 +0200

    fix deps

--- fuzz/fuzz_targets/zlib.rs
+++ fuzz/fuzz_targets/zlib.rs
@@ -22,6 +22,10 @@ fuzz_target!(|data: &[u8]| {
     let mut inf = Inflater::new();
 
     block_on(inf.inflate(&compressed[..], &mut inflated)).unwrap();
+    assert_eq!(data, &inflated[..]);
+});
+
+
 
     let d = Decompress::new_with_window_bits(false, 15);
     let mut zd = ZlibDecoder::new_with_decompress(&compressed[..], d);
@@ -29,5 +33,4 @@ fuzz_target!(|data: &[u8]| {
     zd.read_to_end(&mut z_inflated).unwrap();
 
     assert_eq!(data, &z_inflated[..]);
-    assert_eq!(data, &inflated[..]);
 });
